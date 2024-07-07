76d77562914aabdd9ae83029fa397f6eaab2c127
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2024-07-07 21:06:58 +0800

    update cfg file, email, name, github

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="youtube-suggestions",
-    version="0.2.0",  # Incremented version number
-    author="Your Name",
-    author_email="your.email@example.com",
+    version="0.2.1",
+    author="Ryan Huang",
+    author_email="ryan@stdint.com",
     description="A package to get YouTube search suggestions with proxy support",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/youtube-suggestions",
+    url="https://github.com/ryan-huang1/youtube_suggestions",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "requests>=2.25.1",
